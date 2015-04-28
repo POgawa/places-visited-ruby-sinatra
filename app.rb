c759d0ccdeff6b3cd338652da9cc9b0d26d48970
Paul Ogawa & Leon Zagorouiko <student@epicodus.com>
2015-04-28 12:28:32 -0700

    it adds sinatra framework and display logic

--- app.rb
+++ app.rb
@@ -9,3 +9,17 @@ also_reload('lib/**/*.rb')
 get('/') do
    erb(:index)
 end
+
+get('/output') do
+  @location = params.fetch('location')
+  @description = params.fetch('description')
+  @places = Place.new(@location, @description)
+  @places.save()
+  # @output_location = @places.name()
+  # @output_description = @places.description()
+
+  @all_places = Place.all()
+
+  erb(:output)
+
+end
--- spec/PYB_integration_spec.rb
+++ spec/PYB_integration_spec.rb
@@ -2,3 +2,11 @@ require 'PYB'
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
+
+describe('PYB path', {:type => :feature}) do
+  it('Adds and displays places you have visited, and a description')
+  visit('/')
+  fill_in('location', :with => 'Ukraine')
+  fill_in('description' :with => 'a magical place')
+  click_button('Send')
+  expect(page). have_content('Ukraine', 'a magical place')
